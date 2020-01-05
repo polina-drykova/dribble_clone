c5de23202d71ded760ba79ac2e2eef12eac31aab
Polina Drykova <polina.drykova1988@gmail.com>
2020-01-05 17:54:14 +0700

    associations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # mount_uploader :avatar, PhotoUploader
   has_many :shots, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
